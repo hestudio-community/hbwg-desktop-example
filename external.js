6e045ef5fc756078a0ec2533135c7bfec404fbb1
undefined <zihengdie@hestudio.net>
2024-12-10 23:22:59 +0800

    实现更稳定的更新方案

--- external.js
+++ external.js
@@ -11,12 +11,19 @@ module.exports = {
     logback("Setting wallpaper ...");
     exec(
       `reg add "HKCU\\Control Panel\\Desktop" /v Wallpaper /t REG_SZ /d "${imagePath}" /f`,
-      (error) => {
+      (error, stdout) => {
+        logback(stdout);
         if (error) {
           logback(`Failed to set wallpaper: ${error}`);
         } else {
-          exec(`RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters`);
-          logback("Setting wallpaper done");
+          exec(`RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters`, (error, stdout) => {
+            logback(stdout);
+            if (error) {
+              logback(`Failed to set wallpaper: ${error}`);
+            } else {
+              logback("Setting wallpaper done");
+            }
+          });
         }
       }
     );
